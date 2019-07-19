de3b271f2cf9022437421a8d319d2cd702fb79a0
Văn Lộc <vanloc18021998@gmail.com>
2019-07-19 21:47:16 +0700

    web interface & demo stitcher code

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, send_from_directory, request, session,
 from flask_dropzone import Dropzone
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 import os
+import shutil
+import stitcher
+import uuid
 
 app = Flask(__name__)
 dropzone = Dropzone(app)
@@ -15,7 +18,7 @@ app.config['DROPZONE_PARALLEL_UPLOADS'] = 100
 
 app.config['SECRET_KEY'] = 'UITCSGANG<3CS231'
 
-app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploads/'
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 patch_request_class(app)
@@ -29,36 +32,51 @@ def favicon():
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if "file_names" not in session:
+    if "file_names" not in session or "uuid_folder" not in session:
         session['file_names'] = []
+        session['uuid_folder'] = []
     file_names = session['file_names']
 
     if request.method == 'POST':
+        uuid_folder = uuid.uuid4().hex
+        os.mkdir('./static/uploads/'+uuid_folder)
+        os.mkdir('./static/uploads/'+uuid_folder+'/result')
+        session['uuid_folder'] = uuid_folder
         file_obj = request.files
         for f in file_obj:
             file = request.files.get(f)
             filename = photos.save(
                 file,
+                folder=uuid_folder,
                 name=file.filename
             )
             file_names.append(photos.url(filename))
         session['file_names'] = file_names
         return "uploading..."
-
     return render_template('index.html')
 
 
 @app.route('/results')
 def results():
+    if 'uuid_folder' not in session or session['uuid_folder'] == []:
+        return redirect(url_for('index'))
     if "file_names" not in session or session['file_names'] == []:
         return redirect(url_for('index'))
 
     file_names = session['file_names']
     session.pop('file_names', None)
+    uuid_folder = session['uuid_folder']
+    session.pop('uuid_folder', None)
 
-    # Xu ly tren tap file file_names
-
-    result = '/static/img/cover.jpg'
+    # Xu ly tren tap file file_namess
+    work_dir = './static/uploads/'+uuid_folder+'/'
+    input_file = [f for f in os.listdir(
+        work_dir) if os.path.isfile(os.path.join(work_dir, f))]
+    if len(input_file) >= 2:
+        stitcher.stitching(input_file, uuid_folder)
+    else:
+        return render_template('index.html')
+    result = work_dir + 'result/result.png'
     return render_template('results.html', result=result)
 
 
